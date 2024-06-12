93c83cb9da8c7f979de867d3c389794137fb9d5f
Ritu Verma <ritu1298verma@gmail.com>
2024-06-12 22:56:15 +0530

    Update baseUrl.js

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://mediconnect-gnacf3cbdjbpg0da.eastus-01.azurewebsites.net/";
+const baseUrl = "https://mediconnect-gnacf3cbdjbpg0da.eastus-01.azurewebsites.net";
 
 export default baseUrl;
