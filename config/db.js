e409a0aa25f8d5a5629ed18546a39269fd621787
Ritu Verma <ritu1298verma@gmail.com>
2024-06-12 22:36:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+const path = require('path');
+
+// Load environment variables from .env file
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:deep1507@cluster0.rd0szsg.mongodb.net/');
+    const mongodbUri = process.env.MONGODB_URI;
+
+    await mongoose.connect(mongodbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
