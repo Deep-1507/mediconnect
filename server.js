9321d9e674afbadc1165e86626d7c083b5ee983f
Ritu Verma <ritu1298verma@gmail.com>
2024-06-12 21:33:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 //dotenv conig
 dotenv.config();
@@ -21,6 +22,11 @@ app.use(moragan("dev"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+app.use(express.static("./client/build"));
+app.get("*", (req,res)=>{
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+});
 
 //port
 const port = 8080;
